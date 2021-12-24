c7865da9cf1ae1753a0e8f0041d00c1ad3f3c022
hausen8 <71127242+hausen8@users.noreply.github.com>
2021-12-24 16:01:52 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -17,17 +17,17 @@
 
 // Display
 // #define SSD1306                                       // Uncomment for OLED with SSD1306 controller
-#define SH1106                                        // Uncomment for OLED with SH1106 controller
+// #define SH1106                                        // Uncomment for OLED with SH1106 controller
 // #define HT16K33                                       // Uncomment for 7-Segment LED Display with HT16K33 Controller
 // #define LCD16X2                                       // Uncomment for 16x2 LCD or VFD with I2C Controller
 // #define LCD16X4                                       // Uncomment for 16x4 LCD or VFD with I2C Controller
 
 
 // Features
-#define DRAIN_SYSTEM                                  // Drain system with reversed fast mode when coolant is set to 0
-#define MOMENTARY_SWITCH                              // Uncomment for momentary switches instead of solid state switches
-#define REMOTE_CTRL                                   // Use input #4 for remote mist control from CNC. Momentary switches are required!
-#define EXT_LED                                       // Uncomment for external LEDs indicating active mode (mist, air or spit)
+// #define DRAIN_SYSTEM                                  // Drain system with reversed fast mode when coolant is set to 0
+// #define MOMENTARY_SWITCH                              // Uncomment for momentary switches instead of solid state switches
+// #define REMOTE_CTRL                                   // Use input #4 for remote mist control from CNC. Momentary switches are required!
+// #define EXT_LED                                       // Uncomment for external LEDs indicating active mode (mist, air or spit)
 
 
 // Coolant pump
